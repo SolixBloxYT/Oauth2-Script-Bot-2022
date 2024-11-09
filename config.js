34ea41da6775e1d1c5a607c704ffd916a4eb64a7
Solix <130551399+SolixBloxYT@users.noreply.github.com>
2024-11-09 18:02:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "OTk3NDk3MDM5MDIzNzcxNjk5.Grx5su.orAjygFJuBPH7I2WLtcEN5Ppk2Jh1kdyVk8tlE",
+    token: "MTMwNDc0NzY0MjA4MDcyNzEwMA.GKhlBO.W7yKtKfW4hFw67CUz1uTgCtQevBlgpOUMg7rbU",
     prefix: ";",
-    owners: [""],
-    authLink: "https://discord.com/api/oauth2/authorize?client_id=997497039023771699&redirect_uri=https%3A%2F%2F.repl.co&response_type=code&scope=identify%20guilds.join",
+    owners: ["255841984470712330"],
+    authLink: "https://discord.com/api/oauth2/authorize?client_id=1304747642080727100&redirect_uri=https%3A%2F%2F.repl.co&response_type=code&scope=identify%20guilds.join",
 
 }
